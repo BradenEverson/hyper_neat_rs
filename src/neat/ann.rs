d32cc8c0e89d05937a5a99238e71bca8f84334e5
Braden Everson <bradeneverson@gmail.com>
2024-05-01 13:01:11 -0500

    and outputs

--- src/neat/ann.rs
+++ src/neat/ann.rs
@@ -8,6 +8,7 @@ pub struct ANN {
     species: u32,
     nodes: SlotMap<NodeId, Node>,
     inputs: Vec<NodeId>,
+    inner: Vec<NodeId>,
     outputs: Vec<NodeId>
 }
 
@@ -20,11 +21,13 @@ impl Default for ANN {
 impl ANN {
     pub fn new() -> Self {
         let nodes: SlotMap<NodeId, Node> = SlotMap::with_key();
-        ANN { species: 0, nodes, inputs: vec![], outputs: vec![] }
+        ANN { species: 0, nodes, inputs: vec![], outputs: vec![], inner: vec![] }
     }
+
     pub fn with_species(&mut self, species: u32) {
         self.species = species
     }
+
     pub fn with_inputs(&mut self, input_count: usize) {
         self.inputs = Vec::with_capacity(input_count);
         for i in 0..input_count {
@@ -32,6 +35,15 @@ impl ANN {
             self.inputs[i] = self.insert(node);
         }
     }
+
+    pub fn and_outputs(&mut self, output_count: usize) {
+        self.outputs = Vec::with_capacity(output_count);
+        for i in 0..output_count {
+            let node = Node::default();
+            self.inputs[i] = self.insert(node);
+        }
+    }
+
     fn insert(&mut self, node: Node) -> NodeId {
         self.nodes.insert(node)
     }
