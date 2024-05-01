b28d7c576dcfcf582b49f7e95f82b8dbcf8c9b6a
Braden Everson <bradeneverson@gmail.com>
2024-05-01 12:51:12 -0500

    Input Output

--- src/neat/ann.rs
+++ src/neat/ann.rs
@@ -1,8 +1,25 @@
+use std::default;
+
 use slotmap::SlotMap;
 
 use super::node::{Node, NodeId};
 
 pub struct ANN {
+    species: u32,
     nodes: SlotMap<NodeId, Node>,
+    inputs: Vec<NodeId>,
+    outputs: Vec<NodeId>
 }
 
+impl Default for ANN {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ANN {
+    pub fn new() -> Self {
+        let nodes: SlotMap<NodeId, Node> = SlotMap::with_key();
+        ANN { species: 0, nodes, inputs: vec![], outputs: vec![] }
+    }
+}
