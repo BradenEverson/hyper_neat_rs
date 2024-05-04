22e46f23908ef7a77e57345025df0edc4ec3f19a
Braden Everson <bradeneverson@gmail.com>
2024-05-03 20:43:44 -0500

    Fitness fn

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,8 @@
 use crate::neat::net::{ann::ANN, edge::Edge, node::NodeId};
 
 pub struct ProblemSet {
-    population: Vec<NeatNet>
+    population: Vec<NeatNet>,
+    fitness: Box<dyn Fn(ANN) -> f32>
 }
 
 pub struct NeatNet {
@@ -10,6 +11,17 @@ pub struct NeatNet {
     edges: Vec<Edge>
 }
 
+impl Default for NeatNet {
+    fn default() -> Self {
+        NeatNet::empty()
+    }
+}
+
 impl NeatNet {
+    pub fn empty() -> Self {
+        NeatNet { genome: ANN::new(), nodes: vec![], edges: vec![] }
+    }
+
+
 
 }
